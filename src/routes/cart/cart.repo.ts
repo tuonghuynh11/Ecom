0f2928c1d6ca0437520968a6a4538d02e7d5c82b
Tuong Huynh Manh <huynhmanhtuong1180@gmail.com>
2026-08-23 18:13:27 +0700

    chore: add function to get CartItem by raw Query

--- src/routes/cart/cart.repo.ts
+++ src/routes/cart/cart.repo.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common'
+import { Prisma } from 'src/generated/prisma/client'
 import {
   NotEnoughStockSKUException,
   NotFoundSKUException,
@@ -51,7 +52,7 @@ export class CartRepo {
     ) {
       throw ProductNotFoundException
     }
-    return sku as any
+    return sku
   }
 
   async list({
@@ -65,61 +66,6 @@ export class CartRepo {
     limit: number
     page: number
   }): Promise<GetCartResType> {
-    // const cartItems = await this.prismaService.cartItem
-    //   .findMany({
-    //     where: {
-    //       userId,
-    //       sku: {
-    //         product: {
-    //           deletedAt: null,
-    //           publishedAt: {
-    //             lte: new Date(),
-    //             not: null,
-    //           },
-    //           createdBy: {
-    //             not: null,
-    //           },
-    //         },
-    //       },
-    //     },
-    //     orderBy: {
-    //       updatedAt: 'desc',
-    //     },
-    //     include: {
-    //       sku: {
-    //         include: {
-    //           product: {
-    //             include: {
-    //               productTranslations: {
-    //                 where: languageId === ALL_LANGUAGES_CODE ? { deletedAt: null } : { languageId, deletedAt: null },
-    //               },
-    //               createdBy: true,
-    //             },
-    //           },
-    //         },
-    //       },
-    //     },
-    //   })
-    //   .then((items) => {
-    //     const groupItemsByShop = items.reduce((acc: any, item: any) => {
-    //       if (acc[item.sku.product.createdBy.id]) {
-    //         acc[item.sku.product.createdBy.id].cartItems.push(item)
-    //       } else {
-    //         acc[item.sku.product.createdBy.id] = {
-    //           shop: {
-    //             id: item.sku.product.createdBy.id,
-    //             name: item.sku.product.createdBy.name,
-    //             avatar: item.sku.product.createdBy.avatar,
-    //           },
-    //           cartItems: [item],
-    //         }
-    //       }
-    //     }, {})
-
-    //     const result: CartItemDetailType[] = Object.values(groupItemsByShop)
-    //     return result
-    //   })
-
     const cartItems = (await this.prismaService.cartItem.findMany({
       where: {
         userId,
@@ -181,6 +127,110 @@ export class CartRepo {
     }
   }
 
+  // Using raw SQL query to get cart items grouped by shop
+  async list2({
+    userId,
+    languageId,
+    page,
+    limit,
+  }: {
+    userId: number
+    languageId: string
+    limit: number
+    page: number
+  }): Promise<GetCartResType> {
+    const skip = (page - 1) * limit
+    const take = limit
+    // Đếm tổng số nhóm sản phẩm
+    const totalItems$ = this.prismaService.$queryRaw<{ createdById: number }[]>`
+      SELECT
+        "Product"."createdById"
+      FROM "CartItem"
+      JOIN "SKU" ON "CartItem"."skuId" = "SKU"."id"
+      JOIN "Product" ON "SKU"."productId" = "Product"."id"
+      WHERE "CartItem"."userId" = ${userId}
+        AND "Product"."deletedAt" IS NULL
+        AND "Product"."publishedAt" IS NOT NULL
+        AND "Product"."publishedAt" <= NOW()
+      GROUP BY "Product"."createdById"
+    `
+    const data$ = await this.prismaService.$queryRaw<CartItemDetailType[]>`
+     SELECT
+       "Product"."createdById",
+       json_agg(
+         jsonb_build_object(
+           'id', "CartItem"."id",
+           'quantity', "CartItem"."quantity",
+           'skuId', "CartItem"."skuId",
+           'userId', "CartItem"."userId",
+           'createdAt', "CartItem"."createdAt",
+           'updatedAt', "CartItem"."updatedAt",
+           'sku', jsonb_build_object(
+             'id', "SKU"."id",
+              'value', "SKU"."value",
+              'price', "SKU"."price",
+              'stock', "SKU"."stock",
+              'image', "SKU"."image",
+              'productId', "SKU"."productId",
+              'product', jsonb_build_object(
+                'id', "Product"."id",
+                'publishedAt', "Product"."publishedAt",
+                'name', "Product"."name",
+                'basePrice', "Product"."basePrice",
+                'virtualPrice', "Product"."virtualPrice",
+                'brandId', "Product"."brandId",
+                'images', "Product"."images",
+                'variants', "Product"."variants",
+                'productTranslations', COALESCE((
+                  SELECT json_agg(
+                    jsonb_build_object(
+                      'id', pt."id",
+                      'productId', pt."productId",
+                      'languageId', pt."languageId",
+                      'name', pt."name",
+                      'description', pt."description"
+                    )
+                  ) FILTER (WHERE pt."id" IS NOT NULL)
+                  FROM "ProductTranslation" pt
+                  WHERE pt."productId" = "Product"."id"
+                    AND pt."deletedAt" IS NULL
+                    ${languageId === ALL_LANGUAGES_CODE ? Prisma.sql`` : Prisma.sql`AND pt."languageId" = ${languageId}`}
+                ), '[]'::json)
+              )
+           )
+         )
+       ) AS "cartItems",
+       jsonb_build_object(
+         'id', "User"."id",
+         'name', "User"."name",
+         'avatar', "User"."avatar"
+       ) AS "shop"
+     FROM "CartItem"
+     JOIN "SKU" ON "CartItem"."skuId" = "SKU"."id"
+     JOIN "Product" ON "SKU"."productId" = "Product"."id"
+     LEFT JOIN "ProductTranslation" ON "Product"."id" = "ProductTranslation"."productId"
+       AND "ProductTranslation"."deletedAt" IS NULL
+       ${languageId === ALL_LANGUAGES_CODE ? Prisma.sql`` : Prisma.sql`AND "ProductTranslation"."languageId" = ${languageId}`}
+     LEFT JOIN "User" ON "Product"."createdById" = "User"."id"
+     WHERE "CartItem"."userId" = ${userId}
+        AND "Product"."deletedAt" IS NULL
+        AND "Product"."publishedAt" IS NOT NULL
+        AND "Product"."publishedAt" <= NOW()
+     GROUP BY "Product"."createdById", "User"."id"
+     ORDER BY MAX("CartItem"."updatedAt") DESC
+      LIMIT ${take} 
+      OFFSET ${skip}
+   `
+    // eslint-disable-next-line @typescript-eslint/await-thenable
+    const [data, totalItems] = await Promise.all([data$, totalItems$])
+    return {
+      data,
+      page,
+      limit,
+      totalItems: totalItems.length,
+      totalPages: Math.ceil(totalItems.length / limit),
+    }
+  }
   async create(userId: number, body: AddToCartBodyType): Promise<CartItemType> {
     await this.validateSKU(body.skuId, body.quantity)
 
